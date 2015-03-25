dfe12710972178311de7b8c96cb2e0cd70070b15
anuragpeshne <anurag.peshne@gmail.com>
2015-03-25 22:09:23 +0530

    string diff for numerical analysis

--- utilites.js
+++ utilites.js
@@ -3,6 +3,34 @@ var utilities = {
 		return (typeof(file.split('.')[1]) !== 'undefined') && 
 		file.substring(file.length - 3).toLowerCase() === 'mp3';
 	},
+
+	interestingFields: ['title', 'artist', 'album', 'year', 'composer'],
+
+	calDifference: function(metadata1, metadata2) {
+		var iiF, diff = 0;
+		for(iiF = 0; iiF < interestingFields.length; iiF++) {
+			var field1 = metadata1[interestingFields[iiF]] || '';
+			var field2 = metadata2[interestingFields[iiF]] || '';
+
+			diff = diff + stringDiff(field1, field2);
+		}
+		return diff;
+	},
+
+	stringDiff: function(string1, string2) {
+		var i, j, diff = 0;
+		while(i < string1.length && j < string2.length) {
+			if(string1.charAt(i) !== string2.charAt(j)) {
+				diff++;
+			}
+			i++, j++;
+		}
+
+		return (string1.length - (i - 1) 
+				+ diff
+				+ string2.length - (j - 2)
+				);
+	},
 }
 
 module.exports = utilities;
